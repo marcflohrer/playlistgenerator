5bc1b817f258526cbc2313498d43fff6e0695219
marcflohrer <59135776+marcflohrer@users.noreply.github.com>
2023-01-05 17:35:59 +0100

    recursion replaced with loop

--- MusicOrganizer/Extensions/DirectoryExtensions.cs
+++ MusicOrganizer/Extensions/DirectoryExtensions.cs
@@ -8,14 +8,22 @@ public static class DirectoryExtensions
     public static int DeleteEmptySubDirectories(this DirectoryInfo directoryInfo)
     {
         var deletedEmptyDirs = 0;
-        foreach (var directory in Directory.GetDirectories(directoryInfo.FullName))
+        var directories = new Stack<string>(Directory.GetDirectories(directoryInfo.FullName));
+        while (directories.Count > 0)
         {
-            deletedEmptyDirs += DeleteEmptySubDirectories(new DirectoryInfo(directory));
+            var directory = directories.Pop();
             if (Directory.GetFiles(directory).Length == 0 &&
                 Directory.GetDirectories(directory).Length == 0)
             {
                 Directory.Delete(directory, false);
                 deletedEmptyDirs++;
+            }
+            else
+            {
+                foreach (var subDirectory in Directory.GetDirectories(directory))
+                {
+                    directories.Push(subDirectory);
+                }
             }
         }
         return deletedEmptyDirs;
